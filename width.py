3381590ce42489b8423a083fa8e95fda75ee75cc
Stephen Stoltzfus <stestoltz@comcast.net>
2017-08-17 00:22:51 -0400

    First batch of commands

--- width.py
+++ width.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 try:
     file = sys.argv[1]
@@ -32,14 +33,13 @@ def trans(letter):
             return translation.index(each)
 
 COMMAND = "COMMAND"
-COMMANDS = ("2", "5", "6")
+COMMANDS = (2, 5, 6)
 NUMBER = "NUMBER"
 STRING = "STRING"
 DO = "DO"
 IF = "IF"
 END = "END"
 SEPARATOR = "SEPARATOR"
-OUT = "OUT"
 
 class Token:
     def __init__(self, val, type_):
@@ -65,7 +65,7 @@ class Lexer:
         elif command == 1:
             self.advance()
             return Token(1, END)
-        elif command == 3:
+        elif command == 3 or command == 9:
             self.advance()
             return Token(3, SEPARATOR)
         elif command == 4:
@@ -75,9 +75,6 @@ class Lexer:
         elif command == 8:
             self.advance()
             return Token(8, IF)
-        elif command == 9:
-            self.advance()
-            return Token(9, OUT)
         else:
             return self.read_cmd()
 
@@ -125,22 +122,182 @@ class Lexer:
         return Token(res, STRING)
 
     def read_cmd(self):
-        res = ""
-        while self.char is not None and self.char in COMMANDS and len(res) <= 3:
-            res += self.char
+        command = ""
+        while self.char is not None and trans(self.char) in COMMANDS and len(command) <= 4:
+            command += str(COMMANDS.index(trans(self.char)))
             self.advance()
-
-        command = 0
-        column = 0
-        for char in res[::-1]:
-            command += COMMANDS.index(char) * 3 ** column
-            column += 1
 
         return Token(command, COMMAND)
 
 source = "".join(filter(lambda c: c in chars, source))
 
 stack = []
+backburner = None
+counter = None
+
+
+def set_counter(val):
+    global counter
+    counter = val
+
+    if counter < 0:
+        counter = 0
+
+
+def num_input():
+    inp = input()
+    try:
+        stack.append(int(inp))
+    except ValueError:
+        try:
+            stack.append(float(inp))
+        except ValueError:
+            pass
+
+
+def flip_ends():
+    if len(stack) > 1:
+        stack[0], stack[-1] = stack[-1], stack[0]
+
+commands = {
+    "0": lambda: stack.append(stack[-1]),
+    "1": lambda: stack.append(stack.pop(-2)),
+    "2": lambda: stack.pop(),
+    "00": lambda: set_counter(stack[-1]),
+    "01": lambda: stack.append(len(stack)),
+    "02": lambda: stack.append(input()),
+    "10": lambda: num_input(),
+    "11": lambda: stack.append(str(stack.pop())),
+    "12": lambda: stack.append(int(stack.pop())),
+    "20": lambda: set_counter(counter + 1),
+    "21": lambda: set_counter(counter - 1),
+    "22": lambda: print(stack.pop()),
+    "000": lambda: stack.append(float(stack.pop())),
+    "001": lambda: stack.append(-stack.pop()),
+    "002": lambda: stack.append(not stack.pop()),
+    "010": lambda: stack.append(stack.pop() + stack.pop()),
+    "011": lambda: stack.append(stack.pop() - stack.pop()),
+    "012": lambda: stack.append(stack.pop() / stack.pop()),
+    "020": lambda: stack.append(stack.pop() // stack.pop()),
+    "021": lambda: stack.append(stack.pop() * stack.pop()),
+    "022": lambda: stack.append(stack.pop() % stack.pop()),
+    "100": lambda: stack.append(math.factorial(stack.pop())),
+    "101": lambda: stack.append(str(stack.pop()) + str(stack.pop())),
+    "102": lambda: stack.append(math.pow(stack.pop(), stack.pop())),
+    "110": lambda: stack.append(math.sqrt(stack.pop())),
+    "111": lambda: stack.append(math.log(stack.pop(), stack.pop())),
+    "112": lambda: stack.append(~stack.pop()),
+    "120": lambda: stack.append(stack.pop() | stack.pop()),
+    "121": lambda: stack.append(stack.pop() & stack.pop()),
+    "122": lambda: stack.append(stack.pop() << stack.pop()),
+    "200": lambda: stack.append(stack.pop() >> stack.pop()),
+    "201": lambda: stack.append(stack.pop()[stack.pop()]),
+    "202": lambda: stack.append(str(stack.pop()) * stack.pop()),
+    "210": lambda: stack.append(counter),
+    "211": lambda: set_counter(stack.pop()),
+    "212": lambda: stack.extend(list(str(stack.pop()))),
+    "220": lambda: flip_ends(),
+    "221": lambda: print(stack[-1]),
+    "222": lambda: sys.exit(stack[-1]),
+    "0000": 0,
+    "0001": 0,
+    "0002": 0,
+    "0010": 0,
+    "0011": 0,
+    "0012": 0,
+    "0020": 0,
+    "0021": 0,
+    "0022": 0,
+    "0100": 0,
+    "0101": 0,
+    "0102": 0,
+    "0110": 0,
+    "0111": 0,
+    "0112": 0,
+    "0120": 0,
+    "0121": 0,
+    "0122": 0,
+    "0200": 0,
+    "0201": 0,
+    "0202": 0,
+    "0210": 0,
+    "0211": 0,
+    "0212": 0,
+    "0220": 0,
+    "0221": 0,
+    "0222": 0,
+    "1000": 0,
+    "1001": 0,
+    "1002": 0,
+    "1010": 0,
+    "1011": 0,
+    "1012": 0,
+    "1020": 0,
+    "1021": 0,
+    "1022": 0,
+    "1100": 0,
+    "1101": 0,
+    "1102": 0,
+    "1110": 0,
+    "1111": 0,
+    "1112": 0,
+    "1120": 0,
+    "1121": 0,
+    "1122": 0,
+    "1200": 0,
+    "1201": 0,
+    "1202": 0,
+    "1210": 0,
+    "1211": 0,
+    "1212": 0,
+    "1220": 0,
+    "1221": 0,
+    "1222": 0,
+    "2000": 0,
+    "2001": 0,
+    "2002": 0,
+    "2010": 0,
+    "2011": 0,
+    "2012": 0,
+    "2020": 0,
+    "2021": 0,
+    "2022": 0,
+    "2100": 0,
+    "2101": 0,
+    "2102": 0,
+    "2110": 0,
+    "2111": 0,
+    "2112": 0,
+    "2120": 0,
+    "2121": 0,
+    "2122": 0,
+    "2200": 0,
+    "2201": 0,
+    "2202": 0,
+    "2210": 0,
+    "2211": 0,
+    "2212": 0,
+    "2220": 0,
+    "2221": 0,
+    "2222": 0
+}
+
+
+def run_cmd(name):
+    global stack, counter, backburner
+
+    state = {
+        "stack": stack,
+        "counter": counter,
+        "backburner": backburner
+    }
+
+    try:
+        commands[name]()
+    except IndexError:
+        stack = state[stack]
+        counter = state[counter]
+        backburner = state[backburner]
 
 if source == "":
     print(info)
@@ -162,78 +319,159 @@ else:
             stack.append(cmd.val)
         elif cmd.type == IF:
             pass
-        elif cmd.type == OUT:
-            print(stack.pop())
+        elif cmd.type == COMMAND:
+            run_cmd(cmd.val)
 
         cmd = lexer.read()
 
-        """if cmd == 0:
-            # Do (code until cmd 1) while ToS is truthy
-        elif cmd == 1:
-            # End Do
-        elif cmd == 2:
-            # commands
-        elif cmd == 3:
-            # command separator
-        elif cmd == 4:
-            # push base 10 number, but with width numbers; terminated with original char
-        elif cmd == 5:
-            # commands
-        elif cmd == 6:
-            # commands
-        elif cmd == 7:
-            # push string literal; sets of 2 width numbers equate to index in printable ASCII; terminated with original char
-        elif cmd == 8:
-            # If ToS is truthy, Do
-        elif cmd == 9:
-            # pop and output"""
+    try:
+        sys.exit(stack[-1])
+    except IndexError:
+        pass
+
+    """if cmd == 0:
+        # Do (code until end) counter > 0
+    elif cmd == 1:
+        # End Do
+    elif cmd == 2:
+        # commands
+    elif cmd == 3:
+        # command separator
+    elif cmd == 4:
+        # push base 10 number, but with width numbers; terminated with original char
+    elif cmd == 5:
+        # commands
+    elif cmd == 6:
+        # commands
+    elif cmd == 7:
+        # push string literal; sets of 2 width numbers equate to index in printable ASCII; terminated with original char
+    elif cmd == 8:
+        # If counter > 0, Do; optional else with another cmd 8, Do; End
+    elif cmd == 9:
+        # command separator """
 
 """
-commands (27 of them):
-
 0 duplicate ToS
 1 swap ToS and previous
 2 delete ToS
 
-3 input string
-4 input number
-5 to float
-6 to int
-7 to string
-8 negation
-9 not
+00 set counter to read ToS
+01 push height of stack
+02 input string
+10 input number
+11 to string
+12 to int
+20 inc counter
+21 dec counter
+22 output
 
-10 plus
-11 minus
-12 divide
-13 int div
-14 times
-15 mod
-16 concat
-17 power
-18 root
-19 log n
-20 bitwise not
-21 bitwise or
-22 bitwise and
-23 factorial
-24 index
-25 repeat
-26 exit
+000 to float
+001 negation
+002 not
+010 plus
+011 minus
+012 divide
+020 int div
+021 times
+022 mod
+100 factorial
+101 concat
+102 power
+110 root
+111 log n
+112 bitwise not
+120 bitwise or
+121 bitwise and
+122 left shift
+200 right shift
+201 index
+202 repeat
+210 push counter
+211 set counter to pop ToS
+212 split ToS by ""
+220 swap ToS and bottom
+221 read and output
+222 exit
 
-27 sort stack
-28 reverse stack
-29 swap bottom of stack and top of stack
-30 error
-31 split ToS by ""
-32 height of stack
-33 sum of numbers in stack
-34 product of numbers in stack
-35 put stack on backburner
-36 pull stack from backburner, put on bottom
-37 pull stack from backburner, put on top
-38 split ToS by ", "
-39 is prime
-40 is fib
-
+0000 reverse stack
+0001 sort stack
+0002 swap bottom and top
+0010 
+0011 
+0012 
+0020 sum of numbers in stack
+0021 product of numbers in stack
+0022 
+0100 pop stack to backburner
+0101 push stack from backburner to top of stack
+0102 push stack from backburner to bottom of stack
+0110 pop ToS to backburner
+0111 pop ToS to backburner split on ""
+0112 pop ToS to backburner split on y
+0120 
+0121 
+0122 
+0200 join stack on ToS
+0201 join stack on ""
+0202 join y deep into stack on ToS
+0210 join ToS deep into stack on ""
+0211 
+0212
+0220 split ToS by y
+0221 split ToS by ", "
+0222
+1000 is prime
+1001 is fib
+1002
+1010
+1011
+1012
+1020
+1021
+1022
+1100
+1101
+1102
+1110
+1111
+1112
+1120
+1121
+1122
+1200 set counter to read ToS
+1201
+1202
+1210
+1211
+1212
+1220
+1221
+1222
+2000 floor
+2001 ceil
+2002 round
+2010 length
+2011
+2012
+2020
+2021
+2022
+2100
+2101
+2102
+2110
+2111
+2112
+2120
+2121
+2122
+2200
+2201
+2202
+2210
+2211
+2212
+2220
+2221
+2222 error
 """
